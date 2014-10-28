30d9f95b0b0d2a50957df679e6d2aa04b413e4a8
Bryan Mulvihill <bmulvihill@pinsonault.com>
2014-10-27 20:39:22 -0400

    center linkage

---
+++ lib/agglomerative_clustering/linkage/center.rb
@@ -0,0 +1,20 @@
+module AgglomerativeClustering
+  module Linkage
+    class Center < Base
+
+      def calculate_distance(cluster1, cluster2)
+        point1, point2 = center_point(cluster1), center_point(cluster2)
+        euclidean_distance(point1, point2)
+      end
+
+      def center_point cluster
+        cluster.points.first.zip(*cluster.points[1..cluster.points.size-1]).map { |a,b| (a + b)/cluster.points.size.to_f }
+      end
+
+      def clusters_to_merge
+        @clusters_to_merge ||= []
+      end
+
+    end
+  end
+end
---
+++ spec/lib/agglomerative_clustering/linkage/center_spec.rb
@@ -0,0 +1,23 @@
+describe AgglomerativeClustering::Linkage::Center do
+
+  context '#calculate_distance' do
+    it 'will calculate distance between clusters based on the center distance between points' do
+      center_linkage = AgglomerativeClustering::Linkage::Center.new
+      point1 = FactoryGirl.build(:point, x: 1, y: 1, z: 1)
+      point2 = FactoryGirl.build(:point, x: 2, y: 2, z: 2)
+      point3 = FactoryGirl.build(:point, x: 7, y: 7, z: 7)
+      point4 = FactoryGirl.build(:point, x: 5, y: 5, z: 5)
+      cluster1 = AgglomerativeClustering::Cluster.new(point1)
+      cluster2 = AgglomerativeClustering::Cluster.new(point2)
+      cluster3 = AgglomerativeClustering::Cluster.new(point3)
+      cluster4 = AgglomerativeClustering::Cluster.new(point4)
+      cluster1 = cluster1.merge(cluster2)
+      cluster3 = cluster3.merge(cluster4)
+      center_point1 = Point.new(1.5, 1.5, 1.5)
+      center_point2 = Point.new(6, 6, 6)
+      center_distance = center_linkage.euclidean_distance(center_point1, center_point2)
+      expect(center_linkage.calculate_distance(cluster1, cluster3)).to eql(center_distance)
+    end
+  end
+
+end
